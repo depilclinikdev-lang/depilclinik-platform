6b09fd6fe13d6333ed20a3fd38f5e0ee5109a18b
BrianGoOnZ <brian.gonzalez.itid@unipolidgo.edu.mx>
2026-07-26 18:16:25 -0600

    fix(backup): add default-auth flag to fix mysql dump error 3

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
+set -o pipefail
 
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
 FILENAME="backup-${DB_NAME}-${TIMESTAMP}.sql.gz"
 
 echo "[$(date)] Iniciando respaldo de ${DB_NAME}..."
 
-mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" --protocol=TCP | gzip > "/tmp/${FILENAME}"
+mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" --protocol=TCP "$DB_NAME" | gzip > "/tmp/${FILENAME}"
 
 aws s3 cp "/tmp/${FILENAME}" "s3://${AWS_S3_BUCKET_NAME}/depilclinik/db-backups/${FILENAME}"
 
